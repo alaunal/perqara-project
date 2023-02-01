41b68e7477801a2636cf7ae464f4115c54ae6300
alaunal15 <alaunalkauniyyah3@gmail.com>
2023-02-02 04:15:50 +0700

    testing config

--- nuxt.config.js
+++ nuxt.config.js
@@ -67,8 +67,6 @@ export default {
     "@nuxtjs/dotenv",
   ],
 
-  ssr: false,
-
   // Axios module configuration: https://go.nuxtjs.dev/config-axios
   axios: {
     // Workaround to avoid enforcing hard-coded localhost:3000: https://github.com/nuxt-community/axios-module/issues/308
